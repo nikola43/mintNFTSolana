5a2c9891c50685ab24035bbb7f6c2b677fb5537c
nkt <pauloxti@gmail.com>
2022-06-14 22:41:42 +0100

    custom minting machine

---
+++ programs/candy_machine/src/lib.rs
@@ -0,0 +1,140 @@
+pub mod constants;
+pub mod errors;
+pub mod processor;
+pub mod state;
+pub mod utils;
+
+use anchor_lang::prelude::*;
+pub use errors::CandyError;
+pub use processor::*;
+pub use state::*;
+pub use utils::*;
+declare_id!("cndy3Z4yapfJBmL3ShUp5exZKqR3z33thTzeNMm2gRZ");
+
+#[program]
+pub mod candy_machine {
+    use super::*;
+
+    /*
+    pub fn initialize_candy_machine(
+        ctx: Context<InitializeCandyMachine>,
+        data: CandyMachineData,
+    ) -> Result<()> {
+        handle_initialize_candy_machine(ctx, data)
+    }
+    */
+
+    pub fn initialize_candy_machine(
+        ctx: Context<InitializeCandyMachine>,
+        data: CandyMachineData,
+    ) -> Result<()> {
+        handle_initialize_candy_machine(ctx, data)
+    }
+
+    pub fn mint_nft(
+        ctx: Context<MintNFT>,
+        creator_key: Pubkey,
+        uri: String,
+        title: String,
+    ) -> Result<()> {
+        handle_mint_nft(ctx, creator_key, uri, title)
+    }
+}
+
+/*
+
+    pub fn mint_nft(
+        ctx: Context<MintNFT>,
+        creator_key: Pubkey,
+        uri: String,
+        title: String,
+    ) -> Result<()> {
+        msg!("Initializing Mint Ticket");
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+        msg!("CPI Accounts Assigned");
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        msg!("CPI Program Assigned");
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        msg!("CPI Context Assigned");
+        token::mint_to(cpi_ctx, 1)?;
+        msg!("Token Minted !!!");
+        let account_info = vec![
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        msg!("Account Info Assigned");
+        let creator = vec![
+            mpl_token_metadata::state::Creator {
+                address: creator_key,
+                verified: false,
+                share: 100,
+            },
+            mpl_token_metadata::state::Creator {
+                address: ctx.accounts.mint_authority.key(),
+                verified: false,
+                share: 0,
+            },
+        ];
+        msg!("Creator Assigned");
+        let symbol = std::string::ToString::to_string("symb");
+        invoke(
+            &create_metadata_accounts_v2(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.payer.key(),
+                title,
+                symbol,
+                uri,
+                Some(creator),
+                1,
+                true,
+                false,
+                None,
+                None,
+            ),
+            account_info.as_slice(),
+        )?;
+        msg!("Metadata Account Created !!!");
+        let master_edition_infos = vec![
+            ctx.accounts.master_edition.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        msg!("Master Edition Account Infos Assigned");
+        invoke(
+            &create_master_edition_v3(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.master_edition.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.payer.key(),
+                Some(0),
+            ),
+            master_edition_infos.as_slice(),
+        )?;
+        msg!("Master Edition Nft Minted !!!");
+
+        Ok(())
+    }
+*/
---
+++ programs/candy_machine/src/processor/initialize.rs
@@ -0,0 +1,243 @@
+use crate::{
+    assert_initialized, assert_owned_by, cmp_pubkeys,
+    constants::{CONFIG_ARRAY_START, CONFIG_LINE_SIZE},
+    CandyError, CandyMachine, CandyMachineData,
+};
+use anchor_lang::solana_program::program::invoke;
+use anchor_lang::{prelude::*, Discriminator};
+use anchor_spl::token;
+use anchor_spl::token::{MintTo, Token};
+use mpl_token_metadata::instruction::{create_master_edition_v3, create_metadata_accounts_v2};
+use mpl_token_metadata::state::{MAX_CREATOR_LIMIT, MAX_SYMBOL_LENGTH};
+use spl_token::state::Mint;
+#[derive(Accounts)]
+pub struct MintNFT<'info> {
+    #[account(mut)]
+    pub mint_authority: Signer<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub mint: UncheckedAccount<'info>,
+    // #[account(mut)]
+    pub token_program: Program<'info, Token>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_metadata_program: UncheckedAccount<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub payer: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub rent: AccountInfo<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub master_edition: UncheckedAccount<'info>,
+}
+
+/// Create a new candy machine.
+#[derive(Accounts)]
+#[instruction(data: CandyMachineData)]
+pub struct InitializeCandyMachine<'info> {
+    /// CHECK: account constraints checked in account trait
+    #[account(zero, rent_exempt = skip, constraint = candy_machine.to_account_info().owner == program_id && candy_machine.to_account_info().data_len() >= get_space_for_candy(data)?)]
+    candy_machine: UncheckedAccount<'info>,
+    /// CHECK: wallet can be any account and is not written to or read
+    wallet: UncheckedAccount<'info>,
+    /// CHECK: authority can be any account and is not written to or read
+    authority: UncheckedAccount<'info>,
+    payer: Signer<'info>,
+}
+
+pub fn handle_mint_nft(
+    ctx: Context<MintNFT>,
+    creator_key: Pubkey,
+    uri: String,
+    title: String,
+) -> Result<()> {
+    msg!("Initializing Mint Ticket");
+    let cpi_accounts = MintTo {
+        mint: ctx.accounts.mint.to_account_info(),
+        to: ctx.accounts.token_account.to_account_info(),
+        authority: ctx.accounts.payer.to_account_info(),
+    };
+    msg!("CPI Accounts Assigned");
+    let cpi_program = ctx.accounts.token_program.to_account_info();
+    msg!("CPI Program Assigned");
+    let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+    msg!("CPI Context Assigned");
+    token::mint_to(cpi_ctx, 1)?;
+    msg!("Token Minted !!!");
+    let account_info = vec![
+        ctx.accounts.metadata.to_account_info(),
+        ctx.accounts.mint.to_account_info(),
+        ctx.accounts.mint_authority.to_account_info(),
+        ctx.accounts.payer.to_account_info(),
+        ctx.accounts.token_metadata_program.to_account_info(),
+        ctx.accounts.token_program.to_account_info(),
+        ctx.accounts.system_program.to_account_info(),
+        ctx.accounts.rent.to_account_info(),
+    ];
+    msg!("Account Info Assigned");
+    let creator = vec![
+        mpl_token_metadata::state::Creator {
+            address: creator_key,
+            verified: false,
+            share: 100,
+        },
+        mpl_token_metadata::state::Creator {
+            address: ctx.accounts.mint_authority.key(),
+            verified: false,
+            share: 0,
+        },
+    ];
+    msg!("Creator Assigned");
+    let symbol = std::string::ToString::to_string("symb");
+    invoke(
+        &create_metadata_accounts_v2(
+            ctx.accounts.token_metadata_program.key(),
+            ctx.accounts.metadata.key(),
+            ctx.accounts.mint.key(),
+            ctx.accounts.mint_authority.key(),
+            ctx.accounts.payer.key(),
+            ctx.accounts.payer.key(),
+            title,
+            symbol,
+            uri,
+            Some(creator),
+            1,
+            true,
+            false,
+            None,
+            None,
+        ),
+        account_info.as_slice(),
+    )?;
+    msg!("Metadata Account Created !!!");
+    let master_edition_infos = vec![
+        ctx.accounts.master_edition.to_account_info(),
+        ctx.accounts.mint.to_account_info(),
+        ctx.accounts.mint_authority.to_account_info(),
+        ctx.accounts.payer.to_account_info(),
+        ctx.accounts.metadata.to_account_info(),
+        ctx.accounts.token_metadata_program.to_account_info(),
+        ctx.accounts.token_program.to_account_info(),
+        ctx.accounts.system_program.to_account_info(),
+        ctx.accounts.rent.to_account_info(),
+    ];
+    msg!("Master Edition Account Infos Assigned");
+    invoke(
+        &create_master_edition_v3(
+            ctx.accounts.token_metadata_program.key(),
+            ctx.accounts.master_edition.key(),
+            ctx.accounts.mint.key(),
+            ctx.accounts.payer.key(),
+            ctx.accounts.mint_authority.key(),
+            ctx.accounts.metadata.key(),
+            ctx.accounts.payer.key(),
+            Some(0),
+        ),
+        master_edition_infos.as_slice(),
+    )?;
+    msg!("Master Edition Nft Minted !!!");
+
+    Ok(())
+}
+
+pub fn handle_initialize_candy_machine(
+    ctx: Context<InitializeCandyMachine>,
+    data: CandyMachineData,
+) -> Result<()> {
+    let candy_machine_account = &mut ctx.accounts.candy_machine;
+
+    if data.uuid.len() != 6 {
+        return err!(CandyError::UuidMustBeExactly6Length);
+    }
+
+    let mut candy_machine = CandyMachine {
+        data,
+        authority: ctx.accounts.authority.key(),
+        wallet: ctx.accounts.wallet.key(),
+        token_mint: None,
+        items_redeemed: 0,
+    };
+
+    if !ctx.remaining_accounts.is_empty() {
+        let token_mint_info = &ctx.remaining_accounts[0];
+        let _token_mint: Mint = assert_initialized(token_mint_info)?;
+        let token_account: spl_token::state::Account = assert_initialized(&ctx.accounts.wallet)?;
+
+        assert_owned_by(token_mint_info, &spl_token::id())?;
+        assert_owned_by(&ctx.accounts.wallet, &spl_token::id())?;
+
+        if !cmp_pubkeys(&token_account.mint, &token_mint_info.key()) {
+            return err!(CandyError::MintMismatch);
+        }
+
+        candy_machine.token_mint = Some(*token_mint_info.key);
+    }
+
+    let mut array_of_zeroes = vec![];
+    while array_of_zeroes.len() < MAX_SYMBOL_LENGTH - candy_machine.data.symbol.len() {
+        array_of_zeroes.push(0u8);
+    }
+    let new_symbol =
+        candy_machine.data.symbol.clone() + std::str::from_utf8(&array_of_zeroes).unwrap();
+    candy_machine.data.symbol = new_symbol;
+
+    // - 1 because we are going to be a creator
+    /*
+    if candy_machine.data.creators.len() > MAX_CREATOR_LIMIT - 1 {
+        return err!(CandyError::TooManyCreators);
+    }
+    */
+
+    let mut new_data = CandyMachine::discriminator().try_to_vec().unwrap();
+    new_data.append(&mut candy_machine.try_to_vec().unwrap());
+    let mut data = candy_machine_account.data.borrow_mut();
+    // god forgive me couldnt think of better way to deal with this
+    for i in 0..new_data.len() {
+        data[i] = new_data[i];
+    }
+
+    // only if we are not using hidden settings we will have space for
+    // the config lines
+    if candy_machine.data.hidden_settings.is_none() {
+        let vec_start = CONFIG_ARRAY_START
+            + 4
+            + (candy_machine.data.items_available as usize) * CONFIG_LINE_SIZE;
+        let as_bytes = (candy_machine
+            .data
+            .items_available
+            .checked_div(8)
+            .ok_or(CandyError::NumericalOverflowError)? as u32)
+            .to_le_bytes();
+        for i in 0..4 {
+            data[vec_start + i] = as_bytes[i]
+        }
+    }
+
+    Ok(())
+}
+
+fn get_space_for_candy(data: CandyMachineData) -> Result<usize> {
+    let num = if data.hidden_settings.is_some() {
+        CONFIG_ARRAY_START
+    } else {
+        CONFIG_ARRAY_START
+            + 4
+            + (data.items_available as usize) * CONFIG_LINE_SIZE
+            + 8
+            + 2 * ((data
+                .items_available
+                .checked_div(8)
+                .ok_or(CandyError::NumericalOverflowError)?
+                + 1) as usize)
+    };
+
+    Ok(num)
+}
